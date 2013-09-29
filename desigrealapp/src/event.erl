c8aa31c831d233598507708da299f4369eb66c4c
Marek Tr1p0d Kidon <marek.kidon@gmail.com>
2013-09-30 00:29:16 +0200

    eventy vyreseny

--- desigrealapp/src/event.erl
+++ desigrealapp/src/event.erl
@@ -5,10 +5,44 @@
 	name="",
 	to_go=0}).
 
-loop(loop = #state{server=Server}) ->
+
+normalize(N) ->
+	Limit = 49 * 24 * 60 * 60,
+	[N rem Limit | lists:duplicate(N div Limit, Limit)].
+
+loop(S = #state{server=Server,to_go=[H|T]}) ->
 	receive
 		{Server, Ref, cancel} ->
 			Server ! {Ref, ok}
-	after S#state.to_go * 1000 ->
-			Server ! {done, S#state.name}
+	after H * 1000 ->
+		case T of 
+			[] -> 
+				io:format("exiting"),
+				Server ! {done, S#state.name},
+			_ -> 
+				io:format("continue next"),
+				loop(S#state{to_go=T})
+		end
+	end.
+
+start(EventName, Delay) ->
+	spawn(fun() ->	init(self(), EventName, Delay) end).
+
+start_link(EventName, Delay) ->
+	spawn_link(fun() ->	init(self(), EventName, Delay) end).
+
+init(Pid, EventName, Delay) ->
+	loop(#state{server=Pid,
+			name=EventName,
+			to_go=[5,5]}).
+
+cancel(Pid) ->
+	Ref = erlang:monitor(process, Pid),
+	Pid ! {self(), Ref, cancel},
+	receive
+		{Ref, ok} ->
+			erlang:demonitor(Ref, [flush]),
+			ok;
+		{'DOWN', Ref, process, Pid, _Reason} ->
+			ok
 	end.
